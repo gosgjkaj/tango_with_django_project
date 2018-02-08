75762e758d1d8ee13e60bbfe98ce99658d472e34
Haowen Li <2327962l@student.gla.ac.uk>
2018-02-08 17:15:09 +0000

    chapter9.6 finished but could not register... no such table: rango_userprofile

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -98,6 +98,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
     {
         'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        'OPTIONS': { 'min_length': 6, }
     },
     {
         'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
@@ -106,6 +107,7 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+
 
 
 # Internationalization
